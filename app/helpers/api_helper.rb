aa8f9e81f08e1042b792901cff744627ab18f64f
ecrivenco <eugen.crivenco@sapec.md>
2022-07-19 20:04:11 +0300

    Api Helper Refactor
    Code refactor. Avoid double code

--- app/helpers/api_helper.rb
+++ app/helpers/api_helper.rb
@@ -4,36 +4,19 @@ module ApiHelper
   def self.create_customer(identifier)
     uri = URI.parse("#{SALTEDGE}/customers")
 
-    request = Net::HTTP::Post.new(uri)
-      request.content_type = "application/json"
-      request["Accept"] = "application/json"
-      request["App-Id"] = App_Id
-      request["Secret"] = Secret
-      request.body = JSON.dump({
+      body = JSON.dump({
         "data" => {
           "identifier" => identifier
         }
       })
 
-      req_options = {
-        use_ssl: uri.scheme == "https",
-      }
-
-      Net::HTTP.start(uri.host, uri.port, req_options) do |http|
-        response = http.request(request)
-        JSON.parse(response.body)
-      end
+      generate_and_send_api_request("POST", uri, body)
   end 
 
   def self.create_connection(customer_id, customer_secret, country_code, provider_code)
     uri = URI.parse("#{SALTEDGE}/connections")
-    request = Net::HTTP::Post.new(uri)
-    request.content_type = "application/json"
-    request["Accept"] = "application/json"
-    request["App-Id"] = App_Id
-    request["Secret"] = Secret
-    request["Customer-secret"] = customer_secret
-    request.body = JSON.generate({
+   
+    body = JSON.generate({
       "data" => {
         "customer_id" => customer_id,
         "country_code" => country_code,
@@ -53,26 +36,13 @@ module ApiHelper
       }
     })
     
-    req_options = {
-      use_ssl: uri.scheme == "https",
-    }
-    
-    Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-    response = http.request(request)
-    JSON.parse(response.body)
-    end
+    generate_and_send_api_request("POST", uri, body, customer_secret)
   end
   
   def self.refresh_or_reconnect_connection(connection_id, customer_secret, connection_secret, perform_action)
     uri = perform_action == "refresh" ? URI.parse("#{SALTEDGE}/connection/refresh") : URI.parse("#{SALTEDGE}/connection/reconnect")
-    request = Net::HTTP::Put.new(uri)
-    request.content_type = "application/json"
-    request["Accept"] = "application/json"
-    request["App-Id"] = App_Id
-    request["Secret"] = Secret
-    request["Customer-secret"] = customer_secret
-    request["Connection-secret"] = connection_secret
-    request.body = JSON.generate({
+
+    body = JSON.generate({
       "data" => {
         "id" => connection_id,
         "credentials" => {
@@ -87,30 +57,53 @@ module ApiHelper
         "override_credentials" => true
       }
     })
-    req_options = {
-      use_ssl: uri.scheme == "https",
-    }
-
-    Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
-      response = http.request(request)
-      JSON.parse(response.body)
-    end
+ 
+    generate_and_send_api_request("PUT", uri, body, customer_secret, connection_secret)
   end
 
   def self.remove_connection(connection_id, customer_secret, connection_secret)
     uri = URI.parse("#{SALTEDGE}/connection") 
-    request = Net::HTTP::Delete.new(uri)
-    request.content_type = "application/json"
-    request["Accept"] = "application/json"
-    request["App-Id"] = App_Id
-    request["Secret"] = Secret
-    request["Customer-secret"] = customer_secret
-    request["Connection-secret"] = connection_secret
-    request.body = JSON.generate({
+   
+    body = JSON.generate({
       "data" => {
         "id" => connection_id
       }
     })
+
+    generate_and_send_api_request("DELETE", uri, body, customer_secret, connection_secret)
+  end
+
+  def self.get_accounts()
+
+  end
+
+private
+  def self.generate_and_send_api_request(methodType, uri, body, customer_secret = nil, connection_secret = nil)
+    request = Net::HTTP::Get.new(uri)
+
+    case methodType
+    when "POST"
+      request = Net::HTTP::Post.new(uri)
+    when "PUT"
+      request = Net::HTTP::Put.new(uri)
+    when "DELETE"
+      request = Net::HTTP::Delete.new(uri)
+    end
+
+    request.content_type = "application/json"
+    request["Accept"] = "application/json"
+    request["App-Id"] = App_Id
+    request["Secret"] = Secret
+
+    if(customer_secret != nil)
+      request["Customer-secret"] = customer_secret
+    end
+
+    if(connection_secret != nil)
+      request["Connection-secret"] = connection_secret
+    end
+
+    request.body = body
     req_options = {
       use_ssl: uri.scheme == "https",
     }
@@ -118,6 +111,6 @@ module ApiHelper
      Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
      response = http.request(request)
      JSON.parse(response.body)
-    end
+     end
   end
 end
